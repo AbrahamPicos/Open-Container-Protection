846a6d4c2a27d61d98fbc722bc47110b56881c78
AbrahamPicos <abrahampicos@outlook.com>
2026-07-29 15:52:04 -0700

    First commit
    Mod added.

---
+++ 42.20.0/media/lua/shared/OpContainer.lua
@@ -0,0 +1,119 @@
+-- OpContainer.lua
+-- Licence: CC0-1.0(Visit https://creativecommons.org/publicdomain/zero/1.0/ to view details).
+-- Maintainer: AbrahamPicos.
+-- Contributors: 
+
+-- SOBRESCRIBIENDO FUNCIONES VANILLA:
+-- En shared/Moveables/ISMoveableSpriteProps.lua
+-- En shared/TimedActions/ISDestroyStuffAction.lua
+
+-- Estas modificaciones protegen los contenedores de la forma más fiable posible, negando absolutamente  cualquier acción sobre
+--- ellos, pero no pueden proporcionar indicaciones al usuario.
+-- Por favor, añada mensajes que indiquen al usuario por qué no puede realizar acciones con estos objetos.
+
+local legacyIsValid = ISDestroyStuffAction.isValid
+local legacyCanScrapObject = ISMoveableSpriteProps.canScrapObject
+local legacyCanPickUpMoveableInternal = ISMoveableSpriteProps.canPickUpMoveableInternal
+
+-- Verifica si se puede realizar una acción sobre un contenedor, según el criterio de este mod.
+-- Añadí condiciones aparentemente exesivas, porque vanilla hace comprobaciones que sugieren que los tipos pueden ser diferentes.
+---@param character IsoObject? El personaje que está intentando mover el objeto.
+---@param square IsoGridSquare? La baldosa de mapa donde se encuentra el objeto.
+---@param object IsoObject? El objecto que se está evaluando. Puede ser cualquier cosa basada en IsoObject.
+---@return boolean canAction Si se puede realizar una acción sobre el objeto.
+local function canAction(character, square, object)
+
+	-- Si hay un personaje, y el objeto no es golpeable (lo que significa que no fue hecho por un jugador).
+	if (character and instanceof(character, "IsoGameCharacter")) and (object and not instanceof(object, "IsoThumpable")) then
+		square = square or object:getSquare()
+
+		-- Si el objeto es un contenedor.
+		if object:hasProperty("container") then
+			local safehouse = instanceof(character, "IsoPlayer") and SafeHouse.getSafeHouse(square)
+
+			-- Si el objeto no está en una safehouse, o el personaje no es un jugador que pertenezca a ella, prevenir recogida.
+			if not safehouse or not safehouse:playerAllowed(character) then
+				return false
+			end
+		end
+	end
+
+	return true
+end
+
+-- Verifica si un objeto puede levantarse, pero ahora protege los contenedores del mapa.
+---@param _character IsoPlayer El personaje que está intentando mover el objeto.
+---@param _square IsoGridSquare La baldosa de mapa donde se encuentra el objeto.
+---@param _object IsoObject? El objecto que se está evaluando. Puede ser cualquier cosa basada en IsoObject.
+---@param _isMulti boolean Si el objecto es un objeto multi-sprite.
+---@return boolean canPickUp Si el objeto puede levantarse.
+---@diagnostic disable-next-line: duplicate-set-field
+function ISMoveableSpriteProps:canPickUpMoveableInternal(_character, _square, _object, _isMulti)
+	local canPickUp = legacyCanPickUpMoveableInternal(self, _character, _square, _object, _isMulti)
+
+	-- Si vanilla dice que el objeto puede moverse, verificar si estamos de acuerdo.
+	if canPickUp then
+		canPickUp = canAction(_character, _square, _object)
+	end
+
+	return canPickUp
+end
+
+-- Verifica si un objeto puede desmantelarse, pero ahora protege los contenedores del mapa.
+---@param _character IsoPlayer
+---@return umbrella.ISMoveableSpriteProps.CanScrapResult result
+---@return number chance
+---@return string? perk
+---@diagnostic disable-next-line: duplicate-set-field
+function ISMoveableSpriteProps:canScrapObject(_character)
+	local result, chance, perk = legacyCanScrapObject(self, _character)
+
+	-- Si vanilla dice que el objeto puede desmantelarse, y el jugador no está chetado.
+	if result.canScrap and not _character:isMovablesCheat() then
+		local object = self.object--[[@as IsoObject]] -- Por alguna razón esto no está documentado en Umbrella.
+		local canScrap ---@type boolean
+
+		-- Si el objeto es multi-sprite, verificar todos los objetos del grupo.
+		if self.isMultiSprite then
+
+			-- Buscar nuevamente si alguno de los objetos no puede desmantelarse, pero esta vez usando nuestro criterio.
+			for i, member in ipairs(self:getSpriteGridInfo(object:getSquare(), true)) do
+				local obj = member.object
+
+                canScrap = obj and canAction(_character, nil, obj)
+
+				if not canScrap then
+					break
+				end
+			end
+
+		-- De lo contrario, simplemente verificar si estamos en desacuerdo.
+		else
+			canScrap = canAction(_character, object:getSquare(), object)
+		end
+
+		-- Si estamos en desacuerdo, negar.
+		if not canScrap then
+			perk = nil; chance = 0; result = {canScrap = false}
+		end
+	end
+
+	return result, chance, perk
+end
+
+-- Valida si un objeto debería ser destruído usando una almádena, pero ahora protege los contenedores del mapa.
+---@diagnostic disable-next-line: duplicate-set-field
+function ISDestroyStuffAction:isValid()
+	local isValid = legacyIsValid(self)
+	local object = self.item
+
+	-- Si vanilla dice que el objeto puede destruirse, y el jugador no está chetado, verificar si estamos de acuerdo.
+	if isValid and not self.character:isBuildCheat() then
+		isValid = canAction(self.character, object:getSquare(), self.item)
+	end
+
+	return isValid
+end
+
+-- Esto indudablemente evita que rompan los contenedores leves, pero causa inconsistencias que los usuarios notarán.
+--- Esto está listo para producción, pero para corregir estas inconsistencias, deben sobrescribirse más funciones.
---
+++ README.md
@@ -0,0 +1,38 @@
+# Open Container Protection.
+
+Protege los contenedores del mundo para que los jugadores no puedan romperlos o moverlos, con la intención de proteger el reloot.
+
+Este proyecto surge como una alternativa libre a los mods de protección de contenedores existentes, por lo que puede hacer lo que quiera con el. No es necesario dar ninigún crédito a los colaboradores.
+
+Aunque está inspirado en el mod "Container Protection" de iLusioN, ha sido escrito completamente desde cero, siguiendo un enfoque distinto.
+
+## Características:
+
+1. Extremadamente simple y ligero.
+    - No necesita configuración.
+
+2. Protección de contenedores contra:
+    - Levantarlos al inventario, o al suelo.
+    - Desmantelarlos para obtener materiales.
+    - Demolerlos usando una almádena.
+
+3. Los contenedores están protegidos en:
+    - Interiores, dentro de los edificios.
+    - Exteriores, incluso en medio de la nada.
+
+4. Los contenedores quedan desprotegidos si:
+    - Son maniquíes.
+    - Un jugador los construyó.
+    - Es un administrador usando trampas.
+    - Están dentro de la casa segura del jugador.
+
+##
+
+Las trampas necesarias para saltar las restricciones del mod son:
+
+- `BuildChat`: Para destruir.
+- `MoveableCheat`: Para levantar y desmantelar.
+
+##
+
+Este mod está publicado bajo licencia CC0-1.0, y soporta únicamente la última versión de Proyect Zomboid: 1.20.0.
